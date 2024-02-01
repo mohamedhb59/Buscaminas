cc1233054f4199c0a3a1fc335ded6b8fcad05da3
mohamedhb59 <mohamedhb59@educastur.es>
2024-02-01 18:37:53 +0100

    Actualizacion 5

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-const matriz = [];
+var matriz = [];
 const filas = 10;
 const columnas = 10;
 /*--------------------------------------------------*/
-function inicio(){
+function inicio() {
     generarBotones();
-    for(var i=0; i<filas; i++){
+    for (var i = 0; i < filas; i++) {
         matriz.push([]);
-        for(var j=0; j<columnas; j++){
+        for (var j = 0; j < columnas; j++) {
             matriz[i].push(0);
         }
     }
 
     var minas = 5;
-    while (minas>0){
+    while (minas > 0) {
         var fila = Math.floor(Math.random() * filas);
         var columna = Math.floor(Math.random() * columnas);
-        if(matriz[fila][columna] == 0){
+        if (matriz[fila][columna] == 0) {
             matriz[fila][columna] = 1;
             minas--;
         }
     }
 }
 
-function generarBotones(){
-    var html ="";
-    for(var i=0; i<filas; i++){
+function generarBotones() {
+    var html = "";
+    for (var i = 0; i < filas; i++) {
         html += '<div>';
-        for(var j=0; j<columnas; j++){
-            html += '<button type="button" onclick="destapa(event.target,'+i+','+j+')"></button>';
+        for (var j = 0; j < columnas; j++) {
+            html += '<button type="button" onclick="destapa(event.target,' + i + ',' + j + ')"></button>';
         }
         html += '</div>';
     }
     document.querySelector('#matriz').innerHTML = html;
 }
 
-function destapar(button, fila, columna){
-    if(matriz[fila][columna] == 1){
+function destapar(button, fila, columna) {
+    if (matriz[fila][columna] == 1) {
         button.style.backgroundColor = 'red';
         button.disabled = true;
-        alert("Has perdido");
-    }else if(minaCercana(fila, columna)){
+        alert("Boom");
+    } else if (minaCercana(fila, columna)) {
+        button.style.backgroundColor = 'yellow';
+        button.disabled = true;
+
+    } else {
         button.style.backgroundColor = 'green';
         button.disabled = true;
-       
-    }else{
-        button.style.backgroundColor = 'white';
-        button.disabled = true;
-        
+
     }
-   
-    
+
+
 }
 
-function minaCercana(fila, columna){
-    for(i = fila-1; i<= fila +1; i++){
-        for(j=columna-1; j<=columna+1; j++){
-            if(i>=0 && j>=0 && i<filas && j<columnas && (i!= fila || j!= columna)){
-                if(matriz[i][j] == 1){
+function minaCercana(fila, columna) {
+    for (i = fila - 1; i <= fila + 1; i++) {
+        for (j = columna - 1; j <= columna + 1; j++) {
+            if (i >= 0 && j >= 0 && i < filas && j < columnas && (i != fila || j != columna)) {
+                if (matriz[i][j] == 1) {
                     return true;
                 }
             }
